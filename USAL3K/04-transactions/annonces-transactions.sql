9e087dbb9e4207f844cce4e0d51a147786b1a923
MDevoldere <mdevoldere@users.noreply.github.com>
2022-04-29 10:56:25 +0200

    Update annonces-transactions.sql

--- USAL3K/04-transactions/annonces-transactions.sql
+++ USAL3K/04-transactions/annonces-transactions.sql
@@ -1,7 +1,5 @@
 use annonces;
-
 SET AUTOCOMMIT = 0;
-
 DROP PROCEDURE IF EXISTS buy_product;
 
 DELIMITER $$
@@ -14,22 +12,28 @@ DELIMITER $$
 */
 CREATE PROCEDURE buy_product(IN id_user INT, IN id_product INT) 
 BEGIN
-	
+   
+	DECLARE buyer INT;
+    DECLARE price INT;
+    DECLARE user_amount INT;
+    
     START TRANSACTION;
     
     IF (SELECT product_buyer FROM products WHERE product_id=id_product) IS NOT NULL
     THEN
-		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'LE produit a déjà été vendu !';
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Le produit a déjà été vendu !'; 
     END IF;
     
+    SELECT amount INTO user_amount from users WHERE id=id_user; 
+    
     UPDATE products SET product_buyer=id_user WHERE product_id=id_product;
-    UPDATE users SET amount=amount-(SELECT product_price FROM products WHERE product_id=id_product) WHERE id=id_user;
+	UPDATE users SET amount=amount-(SELECT product_price FROM products WHERE product_id=id_product) WHERE id=id_user;
 
-	IF (SELECT amount from users WHERE id=id_user) < (SELECT product_price FROM products WHERE product_id=id_product) 
+	IF user_amount < (SELECT product_price FROM products WHERE product_id=id_product) 
     THEN
 		ROLLBACK;
 	ELSE
-		COMMIT;
+		COMMIT; 
     END IF;
     
 END $$
